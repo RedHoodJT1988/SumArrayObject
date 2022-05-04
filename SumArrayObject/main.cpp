50a05df750bf2ed7bc3644d1462bd64aeeb62c06
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-05-04 14:42:23 -0700

    update to refactor using overloading within the new function

--- SumArrayObject/main.cpp
+++ SumArrayObject/main.cpp
@@ -3,12 +3,19 @@
 using namespace std;
 
 int sumArray(array<int, 10> myArray);
+void sumArray(array<int, 10> myArray, int& myRef);
 
 int main()
 {
 	array<int, 10> primeArray{ 3, 6, 9, 12, 18, 21, 24, 27, 30, 33 };
 	int result = sumArray(primeArray);
+
+	int refResult;
+	sumArray(primeArray, refResult);
+
 	cout << "The result is " << result << endl;
+	cout << "Result by ref is " << refResult << endl;
+	
 	return 0;
 }
 
@@ -21,4 +28,9 @@ int sumArray(array<int, 10> myArray)
 	}
 
 	return sum;
+}
+
+void sumArray(array<int, 10> myArray, int& myRef)
+{
+	myRef = sumArray(myArray); // Not recursion. This is still overloading
 }
